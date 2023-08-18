4f407b98dc0358b0a555869b20763aedbe2d2430
Liliána Bohus <boli96@gmail.com>
2023-08-18 16:17:28 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# jdbc
+# JDBC
+
+A JDBC a **J**ava **D**ata**b**ase **C**onnectivity-t rövidíti és Java kódból történő 
+adatbázis elérést (és manipulálást) tesz lehetővé.
+
+A JDBC egy ún. API (Application Programmin Interface), avagy egy olyan "programcsomag" pongyolán fogalmazva,
+amit már előre megírtak nekünk és használni tudjuk.
+
+File &rarr; Project Structure &rarr; Libraries &rarr; a felső + jel &rarr; From Maven...
+
+Írd be, hogy `mysql`, nyomj egy entert (és várj, amíg betöltődik) és válaszd ki az egyik mysql-connectort!
+(Pl. `mysql:mysql-connector-java:8.0.30`)
+
+## 1. Összes pizza összes adata
+Futtasd le és értelmzed a már meglévő kódot, ami kilistázza az összes pizza összes adatát!
+
+### 1.a Összes pizza azonosítója és neve
+Módosítsd a programod, hogy a pizzáknak ne írja ki minden adatát, csak az azonosítójukat és nevüket!
+Ne legyenek nem használt változók a kódodban!
+
+### 1.b Összes pizza neve
+Írd át úgy a kódot, hogy a pizzáknak csak a nevei legyenek kiírva és ügyelj arra, hogy továbbra se maradjanak
+felesleges sorok a programodban!
+
+## 2. Olcsó pizzák
+Listázd ki a max. 1000 Ft-ba kerülő pizzák adatait!
+
+## 3. Átlagos pizzaár
+Írd ki, mennyibe kerül átlagosan egy pizza!
+
+## 4. Burzsuj pizzák
+Írd ki, hány darab átlagár feletti pizza van!
---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        // Felvesszük az adatbázisunk adatait
+        String jdbcUrl = "jdbc:mysql://localhost:3306/adatbazis_neve";
+        String username = "root";
+        String password = "jelszo";
+
+        try {
+            // Betöltjük a JDBC Drivert
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Létrehozunk egy kapcsolatot az adatbázissal
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+            // 
+            Statement statement = connection.createStatement();
+
+            // Végrehajtunk egy lekérdezést
+            String sqlQuery = "SELECT AVG(par) avgar FROM pizza";
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
+
+            // A ResultSet-be kerülnek be ilyenkor a lekrédezés eredményei
+            // Ezen végigiterálunk és kiírjuk őket egyesével
+            while (resultSet.next()) {
+
+
+                int ar = resultSet.getInt("avgar");
+
+                System.out.println("Név: " + ar);
+            }
+
+            // Lezárjuk az erőforrásokat, mivel rendkívül elegáns programozók vagyunk
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
