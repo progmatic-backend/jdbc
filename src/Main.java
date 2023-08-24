e5c05a23540fcdcedc2438c254467482773fd0cb
Liliána Bohus <boli96@gmail.com>
2023-08-24 18:01:47 +0200

    comment

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,44 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Felvesszük az adatbázisunk adatait
-        String jdbcUrl = "jdbc:mysql://localhost:3306/adatbazis_neve";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/progmatic";
         String username = "root";
-        String password = "jelszo";
+        String password = "root";
 
         try {
-            // Betöltjük a JDBC Drivert
-            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Létrehozunk egy kapcsolatot az adatbázissal
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+                 Statement statement = connection.createStatement()) {
 
-            // Létrehozunk egy Statementet, amin majd SQL parancsokat tudunk futtatni
-            Statement statement = connection.createStatement();
+//                String insertQuery = "INSERT INTO pizza (pazon, pnev, par) VALUES (9, 'Hawaii', '800')";
+//                int insertedRows = statement.executeUpdate(insertQuery);
+//                System.out.println(insertedRows + " row(s) inserted.");
+//
+//                String updateQuery = "UPDATE pizza SET par = 900 WHERE pnev = 'Hawaii'";
+//                int updatedRows = statement.executeUpdate(updateQuery);
+//                System.out.println(updatedRows + " row(s) updated.");
 
-            // Végrehajtunk egy lekérdezést
-            String sqlQuery = "SELECT * FROM pizza";
-            ResultSet resultSet = statement.executeQuery(sqlQuery);
+                String preparedSql = "SELECT * FROM futar WHERE fazon = ? OR fnev = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(preparedSql)) {
+                    preparedStatement.setInt(1, 3);
+                    preparedStatement.setString(2, "Gyalogkakukk");
 
-            // A ResultSet-be kerülnek be ilyenkor a lekrédezés eredményei
-            // Ezen végigiterálunk és kiírjuk őket egyesével
-            while (resultSet.next()) {
-                int azonosito = resultSet.getInt("pazon");
-                String nev = resultSet.getString("pnev");
-                int ar = resultSet.getInt("par");
+                    try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                        while (resultSet.next()) {
+                            int azonosito = resultSet.getInt("fazon");
+                            String nev = resultSet.getString("fnev");
+                            String tel = resultSet.getString("ftel");
 
-                System.out.println("Azonosító: " + azonosito + ", Név: " + nev + ", Ár:" + ar);
+                            System.out.println("Azonosító: " + azonosito + ", Név: " + nev + ", Telefon: " + tel);
+                        }
+                    }
+                }
             }
 
-            // Lezárjuk az erőforrásokat, mivel rendkívül elegáns programozók vagyunk
-            resultSet.close();
-            statement.close();
-            connection.close();
-
-        } catch (Exception e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
